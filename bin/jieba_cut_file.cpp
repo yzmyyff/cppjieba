94b3cb57e083e6ee4027be8b4accf3636110e919
yzmyyff <rukuang10@gmail.com>
2017-02-14 11:26:49 +0800

    1. 补充readme的说明信息 2. jieba_cut_file在3参数的情况下可以正常执行.

--- bin/jieba_cut_file.cpp
+++ bin/jieba_cut_file.cpp
@@ -5,25 +5,27 @@ using namespace std;
 
 
 int main(int argc, char** argv) {
-  if (argc != 5){
-    cout << "jieba_cut_file cppjieba user_dict in_file out_file" << endl;
+  if (argc != 4){
+    cout << "jieba_cut_file user_dict in_file out_file" << endl;
+    exit(1);
   }
 
-  const string jieba_path(argv[1]);
+  const string jieba_path = "../";
   const string dict_path = jieba_path + "dict/jieba.dict.utf8";
   const string hmm_path = jieba_path + "dict/hmm_model.utf8";
   const string idf_path = jieba_path + "dict/idf.utf8";
   const string stopword_path = jieba_path + "dict/stop_words.utf8";
-  const string user_dict_path(argv[2]);
+  const string user_dict_path(argv[1]);
 
   cppjieba::Jieba jieba(dict_path, hmm_path,
 		  user_dict_path, idf_path, stopword_path);
 
   vector<string> words;
-  ifstream in(argv[3]);
-  ofstream out(argv[4]);
+  ifstream in(argv[2]);
+  ofstream out(argv[3]);
   string line;
   int num_line = 0;
+  cout << "开始分词." << endl;
 
   while (getline(in, line)) {
     jieba.Cut(line, words, true);
